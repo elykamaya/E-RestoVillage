18d7a9f2ba2823d321087788320f4861c01dfc9d
AISYAH1140 <aisyahfanniyatul@gmail.com>
2021-06-18 21:51:39 +0700

    SDGS PHP

---
+++ sdgs.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Resto Village</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/sdgs.css">
+</head>
+<body>
+    <section>
+        <div class="container">
+            <h2 class="title">SDGS E-RESTO VILLAGE</h2>
+            <div class="grid">
+                <div class="item">
+                    <i class="fa fa-desktop"></i>
+                    <h3>Tanpa Kemiskinan</h3>
+                    <p>Memajukan Desa tanpa kemiskinan karena masih banyak warga desa yang kehilangan pekerjaannya diakibatkan Pandemi
+                    Covid-19, membuka lapangan kerja baru</p>
+                </div>
+                <div class="item">
+                    <i class="fa fa-camera"></i>
+                    <h3>Pekerjaan dan ekonomi</h3>
+                    <p>Meningkatkan Pekerjaan dan pertumbuhan ekonomi Desa Kali Anget Situbondo karena masih banyak warga desa yang membutuhkan
+                    pekerjaan dan pertumbuhan ekonomi</p>
+                </div>
+                <div class="item">
+                    <i class="fa fa-mobile"></i>
+                    <h3>Inovasi dan infrastruktur desa</h3>
+                    <p>Memajukan Inovasi dan infrastruktur desa dengan inovasi teknologi yang belum pernah diterapkan dalam perkembangan
+                    desa Kali Anget Situbondo selama ini.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>

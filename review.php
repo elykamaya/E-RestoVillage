9536daa8ecac90a27a1d056e66157feb77e8a506
AISYAH1140 <aisyahfanniyatul@gmail.com>
2021-06-18 21:51:02 +0700

    REVIEW PHP

---
+++ review.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Resto Village</title>
+    <link rel="stylesheet" href="css/review.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    
+</head>
+<body>
+
+<!-- Content Header (Page header) -->
+      <strong>
+        <h1><CENTER><strong>REVIEW CUSTOMER</strong> </CENTER></h1>
+      </strong>
+
+  <div class="wrapper">
+    <div class="box">
+      <i class="fas fa-quote-left quote"></i>
+      <p>Tingkatkan perkembangan websi9te agar website tersebut dapat membantu UMKM di Desa Kali Anget Situbondo.</p>
+      <div class="content">
+        <div class="info">
+          <div class="name">Elyka Maya</div>
+          <div class="job">CUSTOMER / PENDUDUK </div>
+          <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="far fa-star"></i>
+            <i class="far fa-star"></i>
+            <i class="far fa-star"></i>
+          </div>
+        </div>
+        <div class="image">
+          <img src="images/gamer.png" alt="">
+        </div>
+      </div>
+    </div>
+    <div class="box">
+      <i class="fas fa-quote-left quote"></i>
+      <p>Website nya sangat inspiratif dn inovatif, semoga kedepannya bisa memajukan perekonomian di setiap desa di Situbondo</p>
+      <div class="content">
+        <div class="info">
+          <div class="name">Aisyah Fanniya</div>
+          <div class="job">CUSTOMER / PENDUDUK</div>
+          <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="far fa-star"></i>
+            <i class="far fa-star"></i>
+          </div>
+        </div>
+        <div class="image">
+          <img src="images/gamer.png" alt="">
+        </div>
+      </div>
+    </div>
+    <div class="box">
+      <i class="fas fa-quote-left  quote"></i>
+      <p>Inspiratif, inovatif, kreatif semoga website ini menjadi penggerak kalangan muda di pedesaan untuk melek teknologi.</p>
+      <div class="content">
+        <div class="info">
+          <div class="name">JOKOWI</div>
+          <div class="job">PRESIDEN INDONESIA</div>
+          <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="far fa-star"></i>
+          </div>
+        </div>
+        <div class="image">
+          <img src="images/gamer.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+
+</body>
+</html>
+
+

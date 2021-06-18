285934a6377bcecef743dfcaaccca9a00711a0ab
elykamaya <72068131+elykamaya@users.noreply.github.com>
2021-06-18 20:55:04 +0700

    footer

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+  <div class="pull-right hidden-xs"> 
+    <b>Kelompok 19A</b>
+  </div>
+    <strong>Copyright@2021 : Pemrograman Berbasis Web - A -</br>
+    Elyka Maya Ningtiyas - 192410101107</br>
+    Aisyah Fanniyatuh Hayah - 192410101140</strong>
+</footer>
